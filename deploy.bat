05332c40c563ac6a8310c87c36ee398ca4b8d5a3
JoshuaZheng <3107641910@qq.com>
2023-12-28 20:09:31 +0800

    'deploy'

---
+++ deploy.bat
@@ -0,0 +1,15 @@
+@echo off
+echo "------deploying------"
+npm -v && (
+echo "-----building-----"
+npm run build
+
+echo "-----push code-----"
+
+git add .
+git commit -m 'deploy'
+git push
+
+echo "-----over-----"
+)
+pause
